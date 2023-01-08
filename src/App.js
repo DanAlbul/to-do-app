59e3f3aa38833662700ad32ebab2bdb3fe41331d
DanAlbul <daniel.albul@dochq.co.uk>
2023-01-08 22:05:13 +0200

    feat: setup project + add basic layout and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="weekday_wrapper">
+        <div className="today-widget">
+          <h2>
+            Today is Thursday <br /> the 6th of November 2023
+          </h2>
+        </div>
+      </div>
+      <div className="tasks_wrapper">
+        <ul>
+          <li>
+            <div className="task-block">
+              <div className="task-main">
+                <label>
+                  <input
+                    className="task-checkbox filled-in"
+                    type="checkbox"
+                    name="task"
+                  />
+                  <span className="task">
+                    Finish 2nd part of 'Anubis' project during
+                  </span>
+                </label>
+              </div>
+              <span className="task-category work-cat"></span>
+            </div>
+          </li>
+          <li>
+            <div className="task-block">
+              <div className="task-main">
+                <label>
+                  <input
+                    className="task-checkbox filled-in"
+                    type="checkbox"
+                    name="task"
+                  />
+                  <span className="task">Take pills and vitamines</span>
+                </label>
+              </div>
+              <span className="task-category health-cat"></span>
+            </div>
+          </li>
+          <li>
+            <div className="task-block">
+              <div className="task-main">
+                <label>
+                  <input
+                    className="task-checkbox filled-in"
+                    type="checkbox"
+                    name="task"
+                  />
+                  <span className="task">Pick up delivery</span>
+                </label>
+              </div>
+              <span className="task-category personal-cat"></span>
+            </div>
+          </li>
+          <li>
+            <div className="task-block">
+              <div className="task-main">
+                <label>
+                  <input
+                    className="task-checkbox filled-in"
+                    type="checkbox"
+                    name="task"
+                  />
+                  <span className="task">Buy some foods </span>
+                </label>
+              </div>
+              <span className="task-category shopping-cat"></span>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="add_task">
+        <input type="text" name="add_task" placeholder="Write a new task" />
+        <button className="show_completed_btn">L</button>
+      </div>
+    </>
   );
-}
-
-export default App;
+};
