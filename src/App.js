98c09bfd5e40dc7f8eff765b42db51f5dedaa992
DanAlbul <daniel.albul@dochq.co.uk>
2023-01-10 11:08:41 +0200

    feat: devide layout on basic components

--- src/App.js
+++ src/App.js
@@ -1,83 +1,136 @@
-export const App = () => {
+import { useState } from 'react';
+import TASKS from './data/tasks.json';
+/* import { createContext } from 'react';
+ */
+
+/* const TasksContext = createContext({
+  done: true,
+}); */
+
+export const Widget = () => {
+  const date = new Date();
+
+  const weekday = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
+  const D = {
+    day: date.getDay(),
+    weekday: weekday[date.getDay()],
+    month: months[date.getMonth()],
+    year: date.getFullYear(),
+  };
+
+  return (
+    <div className="weekday_wrapper">
+      <div className="today-widget">
+        <h2>{`Today is ${D.weekday}, the ${D.day} day of ${D.month} ${D.year}`}</h2>
+      </div>
+    </div>
+  );
+};
+
+export const TaskItem = ({ content, category }) => {
+  const [completed, setCompleted] = useState(false);
+
+  /*   useEffect(() => {
+
+
+  }, completed) */
+
+  function toggleStatus() {
+    setCompleted(!completed);
+  }
+
+  return (
+    <div className="task-block">
+      <div className="task-main">
+        <label>
+          <input
+            onChange={toggleStatus}
+            className="task-checkbox filled-in"
+            type="checkbox"
+            name="task"
+          />
+          <span className={`${completed ? 'completed' : ''} task`}>
+            {content}
+          </span>
+        </label>
+      </div>
+      <span className={`task-category ${category}`}></span>
+    </div>
+  );
+};
+
+export const TasksList = ({ tasks }) => {
+  //const [listType, setListType] = useState();
+
+  //const taskType = useContext(TasksContext);
+
+  const tasksList = [];
+
+  tasks.forEach((task) => {
+    tasksList.push(
+      <li key={task.category}>
+        <TaskItem content={task.content} category={task.category} />
+      </li>
+    );
+  });
+
+  return (
+    <div className="tasks_wrapper">
+      {/* <TasksContext.Provider value={listType}> */}
+      <ul>{tasksList}</ul>
+      {/*  </TasksContext.Provider> */}
+    </div>
+  );
+};
+
+export const TasksManagement = () => {
+  return (
+    <div className="tasks-management">
+      <input
+        type="text"
+        name="tasks-management"
+        placeholder="Write a new task"
+      />
+      <button className="show_completed_btn">L</button>
+    </div>
+  );
+};
+
+export const ToDoApp = ({ tasks }) => {
+ 
   return (
     <>
-      <div className="weekday_wrapper">
-        <div className="today-widget">
-          <h2>
-            Today is Thursday <br /> the 6th of November 2023
-          </h2>
-        </div>
-      </div>
-      <div className="tasks_wrapper">
-        <ul>
-          <li>
-            <div className="task-block">
-              <div className="task-main">
-                <label>
-                  <input
-                    className="task-checkbox filled-in"
-                    type="checkbox"
-                    name="task"
-                  />
-                  <span className="task">
-                    Finish 2nd part of 'Anubis' project during
-                  </span>
-                </label>
-              </div>
-              <span className="task-category work-cat"></span>
-            </div>
-          </li>
-          <li>
-            <div className="task-block">
-              <div className="task-main">
-                <label>
-                  <input
-                    className="task-checkbox filled-in"
-                    type="checkbox"
-                    name="task"
-                  />
-                  <span className="task">Take pills and vitamines</span>
-                </label>
-              </div>
-              <span className="task-category health-cat"></span>
-            </div>
-          </li>
-          <li>
-            <div className="task-block">
-              <div className="task-main">
-                <label>
-                  <input
-                    className="task-checkbox filled-in"
-                    type="checkbox"
-                    name="task"
-                  />
-                  <span className="task">Pick up delivery</span>
-                </label>
-              </div>
-              <span className="task-category personal-cat"></span>
-            </div>
-          </li>
-          <li>
-            <div className="task-block">
-              <div className="task-main">
-                <label>
-                  <input
-                    className="task-checkbox filled-in"
-                    type="checkbox"
-                    name="task"
-                  />
-                  <span className="task">Buy some foods </span>
-                </label>
-              </div>
-              <span className="task-category shopping-cat"></span>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div className="add_task">
-        <input type="text" name="add_task" placeholder="Write a new task" />
-        <button className="show_completed_btn">L</button>
-      </div>
+      <Widget data={`Today is Thursday : the 6th of November 2023`} />
+      <TasksList tasks={tasks} />
+      <TasksManagement />
     </>
   );
+};
+
+export const App = () => {
+  return <ToDoApp tasks={TASKS} />;
 };
