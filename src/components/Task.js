32244a7a7a981c4f24484bc6f028f4b8da61cc5c
DanAlbul <daniel.albul.biz@gmail.com>
2023-02-07 20:06:05 +0200

    feat: add feature to show full task text on hover, add possibility to select the text by removing mui checkboxes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,29 +1,37 @@
-import { useState, useContext } from 'react';
-import { useLocalStorage } from './useLocalStorage';
-import { TasksContext } from './Context';
+import { useState, useContext } from 'react'
+import { useLocalStorage } from './useLocalStorage'
+import { TasksContext } from './Context'
 
 export const TaskItem = (props) => {
-  const [completed, setCompleted] = useState(props.isCompleted);
-  const [items, setItems] = useLocalStorage('TASKS_LIST', []);
-  const { views, isUpdated, setIsUpdated } = useContext(TasksContext);
+  const [completed, setCompleted] = useState(props.isCompleted)
+  const [items, setItems] = useLocalStorage('TASKS_LIST', [])
+  const { views, isUpdated, setIsUpdated } = useContext(TasksContext)
+  const [mouseOver, setMouseOver] = useState(false)
+
+  const handleMouseEnter = () => {
+    setMouseOver(true)
+  }
+
+  const handleMouseOut = () => {
+    setMouseOver(false)
+  }
 
   function toggleStatus(e) {
-    const tasks = JSON.parse(localStorage.getItem('TASKS_LIST'));
-    const task = tasks.find((task) => task.id === props.id);
-    task.completed = !completed;
+    const tasks = JSON.parse(localStorage.getItem('TASKS_LIST'))
+    const task = tasks.find((task) => task.id === props.id)
+    task.completed = !completed
 
-    tasks[tasks.indexOf(task)] = task;
+    tasks[tasks.indexOf(task)] = task
 
-    setItems(tasks);
+    setItems(tasks)
 
-    setCompleted(!completed);
-    setIsUpdated(Date.now());
+    setCompleted(!completed)
+    setIsUpdated(Date.now())
   }
 
   return (
     <div
       className={`${completed ? 'completed' : ''} task-block`}
-
       style={
         views.includes('completed')
           ? {
@@ -32,31 +40,32 @@ export const TaskItem = (props) => {
               transition: 'text-decoration 100ms',
             }
           : {}
-      }
-    >
-      <div className="task-main">
-        <label title={props.content}>
-          <input
-            onChange={toggleStatus}
-            className="task-checkbox filled-in"
-            type="checkbox"
-            name="task"
-            checked={completed}
-          />
-          <span className="task">
-            <span>{props.content}</span>
-          </span>
-        </label>
-      </div>
+      }>
+      {/*   <div className='task-main'> */}
+      <input
+        onChange={toggleStatus}
+        className='task-checkbox'
+        type='checkbox'
+        name='task'
+        checked={completed}
+      />
+      <label
+        onMouseEnter={handleMouseEnter}
+        onMouseOut={handleMouseOut}
+        className={`task${mouseOver ? ' active-task' : ''}`}
+        /*  title={props.content} */
+      >
+        {props.content}
+      </label>
+      {/*  </div> */}
       <span className={'date-of-creation'}>{props.created}</span>
       <span
-        className="task-category"
+        className='task-category'
         style={{ backgroundColor: props.category.color }}
-        datacontent={props.category.type}
-      ></span>
+        datacontent={props.category.type}></span>
     </div>
-  );
-};
+  )
+}
 
 /* function showLineThrough(e) {
   e.target.style.textDecorationColor = 'black';
